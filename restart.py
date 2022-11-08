0b81f92618a9dfd1cd653c41f53ba827acc1a886
comHeop <1533115017@qq.com>
2022-11-08 21:59:23 +0800

    更新错误
    Signed-off-by: comHeop <1533115017@qq.com>

--- restart.py
+++ restart.py
@@ -5,7 +5,7 @@ import os
 schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')"))
 schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')"))
 schedule.every().day.at("04:01").do(os.system("cd InceptionQQbot-master/ ; nb run"))
-schedule.every().day.at("04:02").do(os.system("cd cd go-cqhttp/ ; go-cqhttp"))
+schedule.every().day.at("04:02").do(os.system("cd go-cqhttp/ ; go-cqhttp"))
 
 while True:
     schedule.run_pending()  # 运行所有可运行的任务
