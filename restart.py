f27a359f5191927256a363fc9e0290e755d3eaf3
comHeop <1533115017@qq.com>
2022-11-08 21:47:06 +0800

    更新了错误
    Signed-off-by: comHeop <1533115017@qq.com>

--- restart.py
+++ restart.py
@@ -1,16 +1,11 @@
-# -*- coding: UTF-8 -*-
 import schedule
 import os
 
-
-def stra():
-    os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')")
-    os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')")
-    os.system('cd InceptionQQbot-master/ ; nb run')
-    os.system('cd cd go-cqhttp/ ; go-cqhttp')
-
-
-schedule.every().day.at("04:00").do(stra)  # 每天4点重启
+# 每天4点重启
+schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')"))
+schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')"))
+schedule.every().day.at("04:01").do(os.system("cd InceptionQQbot-master/ ; nb run"))
+schedule.every().day.at("04:02").do(os.system("cd cd go-cqhttp/ ; go-cqhttp"))
 
 while True:
     schedule.run_pending()  # 运行所有可运行的任务
