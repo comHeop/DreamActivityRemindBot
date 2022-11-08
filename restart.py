58380bc705b3e52d298cdcbfc21897eaf3347b81
comHeop <1533115017@qq.com>
2022-11-08 22:40:56 +0800

    修复了重启错误
    Signed-off-by: comHeop <1533115017@qq.com>

--- restart.py
+++ restart.py
@@ -1,11 +1,31 @@
+import threading
+import time
 import schedule
 import os
 
-# 每天4点重启
-schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')"))
-schedule.every().day.at("04:00").do(os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')"))
-schedule.every().day.at("04:01").do(os.system("cd InceptionQQbot-master/ ; nb run"))
-schedule.every().day.at("04:02").do(os.system("cd go-cqhttp/ ; go-cqhttp"))
 
+def os1():
+    os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')")
+    os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')")
+
+
+def run_threaded(job_func):
+     job_thread = threading.Thread(target=job_func)
+     job_thread.start()
+
+
+def os2():
+    os.system("cd / ; cd /QQ-bot/go-cqhttp/ ; go-cqhttp")
+
+
+def os3():
+    os.system("cd / ; cd /QQ-bot/InceptionQQbot-master/ ; nb run")
+
+
+# 每天重启
+schedule.every().day.at("01:00").do(run_threaded, os1)
+schedule.every().day.at("06:30").do(run_threaded, os2)
+schedule.every().day.at("06:30").do(run_threaded, os3)
 while True:
-    schedule.run_pending()  # 运行所有可运行的任务
+    schedule.run_pending()
+    time.sleep(60)
