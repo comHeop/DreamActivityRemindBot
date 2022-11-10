b400bf13e3f520f27a1b488a53ce752a6352c4ca
comHeop <1533115017@qq.com>
2022-11-11 00:12:23 +0800

    更新后restart可直接启动服务
    Signed-off-by: comHeop <1533115017@qq.com>

--- restart.py
+++ restart.py
@@ -4,6 +4,7 @@ import schedule
 import os
 
 
+
 def os1():
     os.system("kill -9  $(ps aux | grep go-cqhttp | awk '{ print $2}')")
     os.system("kill -9  $(ps aux | grep python3.8 | awk '{ print $2}')")
@@ -16,11 +17,20 @@ def run_threaded(job_func):
 
 def os2():
     os.system("cd / ; cd /QQ-bot/go-cqhttp/ ; go-cqhttp")
+    print('2')
 
 
 def os3():
     os.system("cd / ; cd /QQ-bot/InceptionQQbot-master/ ; nb run")
 
+
+threads = []  # 创建一个进程池
+t = threading.Thread(target=os2, args=())
+t1 = threading.Thread(target=os3, args=())
+threads.append(t)  # 添加线程到线程组
+threads.append(t1)  # 添加线程到线程组
+for t in threads:
+    t.start()
 
 # 每天重启
 schedule.every().day.at("01:00").do(run_threaded, os1)
