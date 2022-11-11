ce1845a87bf36f5f3627d5caed94b581a0bada6f
comHeop <1533115017@qq.com>
2022-11-11 10:49:32 +0800

    更新错误 22/11/11 Signed-off-by: comHeop <1533115017@qq.com>

--- restart.py
+++ restart.py
@@ -17,23 +17,13 @@ def run_threaded(job_func):
 
 def os2():
     os.system("cd / ; cd /QQ-bot/go-cqhttp/ ; go-cqhttp")
-    print('2')
 
 
 def os3():
     os.system("cd / ; cd /QQ-bot/InceptionQQbot-master/ ; nb run")
 
-
-threads = []  # 创建一个进程池
-t = threading.Thread(target=os2, args=())
-t1 = threading.Thread(target=os3, args=())
-threads.append(t)  # 添加线程到线程组
-threads.append(t1)  # 添加线程到线程组
-for t in threads:
-    t.start()
-
 # 每天重启
-schedule.every().day.at("01:00").do(run_threaded, os1)
+schedule.every().day.at("00:30").do(run_threaded, os1)
 schedule.every().day.at("06:30").do(run_threaded, os2)
 schedule.every().day.at("06:30").do(run_threaded, os3)
 while True:
