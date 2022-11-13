81f2f7f0c9a8c1638720daab0f37fd4b42818252
comHeop <1533115017@qq.com>
2022-11-13 13:47:09 +0800

    改动错误
    Signed-off-by: comHeop <1533115017@qq.com>

--- cj/到梦空间抢活动/config.py
+++ cj/到梦空间抢活动/config.py
@@ -216,7 +216,7 @@ def updateConfiguration():
     for name in os.listdir('cj/到梦空间抢活动/数据/login'):
         with open('cj/到梦空间抢活动/数据/login/' + name, encoding='utf-8') as file:
             content = file.read()
-            if content != '':
+            if ',' in content:
                 parameter.login_s[name.replace('.txt', '')] = 'd=' + content
                 file.close()
             else:
